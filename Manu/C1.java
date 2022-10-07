91c56c1ff48fc85d59ed8f34b5eda3ac34229290
AMIT <AMIT@rd>
2022-10-07 13:55:24 +0530

    CR

---
+++ Manu/C1.java
@@ -0,0 +1,38 @@
+package Manu;
+
+//import java.util.Scanner;
+
+public class C1 {
+
+	public static void main(String[] args) {
+
+		int i;
+
+		for(i =1; i<=20; i++) {
+
+			System.out.println(i);
+		}
+
+		/*Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		int sum = 0;
+
+		for(int i = 1; i<=n; i++) {
+
+			sum = sum + i;
+
+			System.out.println(sum);
+			}
+
+		// Period/ Padhe
+		for(int i = 1; i<=10; i++) {
+
+			System.out.println(n*i);
+		}*/
+
+
+	}
+
+
+}
